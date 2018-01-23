eb2118ae132dcab6fc78f3871a6d058f4ab72416
Dave Letorey <dletorey@squiz.co.uk>
2018-01-23 16:34:34 +0000

    completed Show The Sign

--- js/tasks.js
+++ js/tasks.js
@@ -85,3 +85,13 @@ if (question == "ECMAScript") {
 }
 
  */
+/* Show the sign https://javascript.info/ifelse#show-the-sign */
+
+let num = prompt("Enter a number",0);
+if (num > 0) {
+    alert("1");
+} else if (num < 0){
+    alert("-1");
+} else {
+    alert("0");
+}
