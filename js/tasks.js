9ce68f83b16d6eba85c14fc66c1ce9d84e27a1be
Dave Letorey <dletorey@squiz.co.uk>
2018-01-24 14:42:11 +0000

    completed Rewrite if to be switch

--- js/tasks.js
+++ js/tasks.js
@@ -195,3 +195,35 @@ if (browser === "Edge") {
     alert ("We hope the page looks OK!");
 }
 */
+
+/* Rewrite if to be switch - https://javascript.info/switch#rewrite-if-into-switch */
+
+// let a = +prompt('a?', '');
+// 
+// if (a == 0) {
+//   alert( 0 );
+// }
+// if (a == 1) {
+//   alert( 1 );
+// }
+// 
+// if (a == 2 || a == 3) {
+//   alert( '2,3' );
+// }
+
+let a = +prompt('a?', '');
+switch (a) {
+    case 0 : {
+        alert("0");
+        break;
+    }
+    case 1 : {
+        alert("1")
+        break;
+    }
+    case 2:
+    case 3: {
+        alert("2 or 3");
+        break;
+    }
+}
