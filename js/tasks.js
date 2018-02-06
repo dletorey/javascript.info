a85c47ae670ed1d5e334b98f988f23a7f7f2db97
Dave Letorey <dletorey@squiz.co.uk>
2018-02-06 15:04:35 +0000

    check for emptiness task

--- js/tasks.js
+++ js/tasks.js
@@ -267,3 +267,19 @@ delete user.name;
 alert (user.name);
 */
 
+/* check for emptiness  */
+
+function isEmpty(obj) {
+    for (let key in obj) {
+      return false;
+    }
+    return true;
+  }
+  
+  let schedule = {};
+
+  alert( isEmpty(schedule) ); // true
+
+  schedule["8:30"] = "get up";
+
+  alert( isEmpty(schedule) ); // false
