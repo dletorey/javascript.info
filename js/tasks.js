6d6ddefa914e0d9cba6283ded4ac6bdc9de1a59c
Dave Letorey <dave@squiz.co.uk>
2018-07-24 15:58:01 +0100

    adding calculator task

--- js/tasks.js
+++ js/tasks.js
@@ -318,3 +318,23 @@ let menu = {
     title: "My menu"
   };
    */
+
+  /* Create Calculator https://javascript.info/object-methods#create-a-calculator */
+
+  let calculator = {
+    sum() {
+        return this.a + this.b;
+    },
+    mul() {
+        return this.a * this.b;
+    },
+    read() {
+        this.a = +prompt('a?', 0);
+        this.b = +prompt('b?', 0);
+    }
+  };
+  
+  calculator.read();
+  alert( calculator.sum() );
+  alert( calculator.mul() );
+  
