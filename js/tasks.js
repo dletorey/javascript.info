d7ea751aa45c08dde2ebaa91e66be71778fb9eb2
Dave Letorey <dave@squiz.co.uk>
2018-07-30 13:47:34 +0100

    Repeat until the input is a number https://javascript.info/number#repeat-until-the-input-is-a-number

--- js/tasks.js
+++ js/tasks.js
@@ -356,7 +356,18 @@ let menu = {
   }; 
    */
 
-/* Sum numbers from visitor */
+/* Sum numbers from visitor 
 let a = +prompt("1st number?");
 let b = +prompt("2nd number?");
 alert(a + b);
+*/
+/* Repeat until the input is a number https://javascript.info/number#repeat-until-the-input-is-a-number */
+function readNumber(){
+    let num;
+    do {
+      num = prompt("Enter a number please?", 0);
+    } while ( !isFinite(num) );
+    if (num === null || num === '') return null;
+    return +num;
+  };
+  alert (`Read ${readNumber()}`);
